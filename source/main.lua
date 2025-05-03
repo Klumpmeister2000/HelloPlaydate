93a64b8dc6528d24fa2e9722de5762b4973d0150
Klumpmeister2000 <christian@klumpmeister.net>
2025-05-03 16:40:08 -0400

    Update build time in pdxinfo and enhance playdate.update function for movement controls

--- source/main.lua
+++ source/main.lua
@@ -1,7 +1,22 @@
 local updates = 0
+local x = 40
+local y = 40
 
 function playdate.update()
     updates = updates + 1
+
+    if playdate.buttonJustPressed(playdate.kButtonUp) then
+        y -= 2
+    end
+    if playdate.buttonJustPressed(playdate.kButtonDown) then
+        y += 2
+    end
+    if playdate.buttonJustPressed(playdate.kButtonLeft) then
+        x -= 2
+    end
+    if playdate.buttonJustPressed(playdate.kButtonRight) then
+        x += 2
+    end
 
     playdate.graphics.clear()
     playdate.graphics.drawText("Hello, Playdate!", 40, 40)
